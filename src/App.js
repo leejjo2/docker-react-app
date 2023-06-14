b4a00246db9e663df974ad221e9b33b753dee55f
jameslee <jungjo4247@naver.com>
2023-06-14 14:17:17 +0900

    feat : docker-react-app docker & nginx

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React !!! test 222
+          learn react
         </a>
       </header>
     </div>
